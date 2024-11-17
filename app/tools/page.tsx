56efa9dfad1740ab169400797a1dfd1f8b20a81f
Jordan Williams <jordanmhizcleo@gmail.com>
2024-11-17 22:25:55 +0100

    feat: tools and icons

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,5 +1,13 @@
 import Bio from "@/components/common/bio";
 import Navbar from "@/components/common/navbar";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { tools } from "@/lib/details";
 
 export default function Page() {
   return (
@@ -13,11 +21,32 @@ export default function Page() {
           <h1 className="font-medium text-2xl ">Tools</h1>
           <p className="leading-7">
             {" "}
-            My primary stack is Typescript, but I have used a variety of other
-            languages and tools over the years, like JAVA and C#. Here are some
-            of the tools I have used and am comfortable with.
+            My primary stacks are Typescript and Next JS. I love how simple
+            next.js can be...I have used a variety of other languages and tools
+            over the years, like JAVA (my first love) and C#. Here are some of
+            the tools I have used and am comfortable with.
           </p>
-          {/* hello world */}
+
+          <div className="h-6" />
+
+          <div className="lg:grid lg:grid-cols-2 gap-4">
+            {tools.map((tool) => (
+              <Card key={tool.title}>
+                <CardHeader className="space-y-4">
+                  <Image
+                    src={tool.logo}
+                    alt={tool.title}
+                    width={50}
+                    height={50}
+                  />
+                  <CardTitle className="font-medium text-2xl">
+                    {tool.title}
+                  </CardTitle>
+                  <CardDescription>{tool.description}</CardDescription>
+                </CardHeader>
+              </Card>
+            ))}
+          </div>
         </section>
       </main>
     </div>
--- lib/details.tsx
+++ lib/details.tsx
@@ -27,3 +27,29 @@ export const socials = [
     key: "rss",
   },
 ];
+
+export const tools = [
+  {
+    title: "Next.JS",
+    description:
+      "Next.JS is a...and my go-to for full-stack web development, I currently use it at work and personally for web-based projects",
+    logo: "/next_js.svg",
+  },
+  {
+    title: "Typescript",
+    description:
+      "Even though I am still learning the language, I have come to understand the core functionalities. And it has become one of my favourite tools to use.",
+    logo: "/typescript.png",
+  },
+  {
+    title: "MongoDB",
+    description: "What's a full-stack application without a database...",
+    logo: "/mongodb.svg",
+  },
+  {
+    title: "Flutter",
+    description:
+      "i feel this gets a lot of hate, don't know why...i have some cool projects with this amazing tool",
+    logo: "/flutter.svg",
+  },
+];
